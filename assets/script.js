85197bd460c5f3d108c2efa4cff9b9f19c8bf68f
Leandra1 <leandrasilvap074@gmail.com>
2022-05-17 20:05:21 -0300

    envio de codigo

--- assets/script.js
+++ assets/script.js
@@ -1,23 +1,21 @@
 
-var num1
-var num2
 function numbers(number){
 
     var num = document.getElementById('print').innerHTML
 
     var aux = document.getElementById('print').innerHTML= num+number
 
-    document.getElementById('print').innerHTML = aux
+}
 
-    num1 = parseInt(aux)
+function back(){
 
-   
+    document.getElementById('print').innerHTML= " "
+
 }
 
 
-function operacao(op){
+function resultado(){
 
-    document.getElementById('print').innerHTML= num1 + ' + '
-    //num2 = document.getElementById('print').innerHTML.split()
-    //ver a questão do segundo numero da operação usando o split e ver como usar split pelo link do slide de js do gsa
+    var result = document.getElementById('print').innerHTML
+    document.getElementById('print').innerHTML =  eval(result)
 }
